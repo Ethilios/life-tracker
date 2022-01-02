17ffef208e692eaa6fddbe5e17f18e2c90c71f7d
Ethilios <ethilios@protonmail.com>
2022-01-02 00:51:23 +0000

    Adds Chakra and font

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,20 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Montserrat',
+    body: 'Montserrat'
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
