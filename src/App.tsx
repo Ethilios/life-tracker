53285b952166c95cc3490ebfabb6dc337bad593b
Ethilios <ethilios@protonmail.com>
2022-01-02 01:17:24 +0000

    init code changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { Box, Container, Heading, Text } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Container
+      className='main-container'
+      centerContent
+    >
+      <Box padding='4'>
+        <Heading>Life Tracker</Heading>
+      </Box>
+      <Box padding='4'>
+        <Text fontSize='xl'>Tracked Data goes here!</Text>
+      </Box>
+    </Container>
   )
 }
 
